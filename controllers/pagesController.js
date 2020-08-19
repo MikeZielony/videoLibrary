0a4b24532cbd1f4e753c4a7b4351b7a7f732256d
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-08-19 19:54:35 +0200

    Refactor code

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -70,43 +70,10 @@ exports.iframe = (req, res) => {
     console.log('disconnected from the in-memory SQlite database.');
 };
 
-
-
 exports.shit = (req, res) => {
     res.render('shit');
 };
 
 exports.delete = (req, res) => {
     res.render('delete');
-};
-
-exports.search5 = (req, res) => {
-
-    // open the database
-
-    let db = new sqlite3.Database('dataBase/movies', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Connected to the in-memory SQlite database.');
-    });
-
-    let sql = `SELECT * FROM movies
-                ORDER BY random() ASC
-                    LIMIT 5;`;
-
-    db.all(sql, (err, row) => {
-        if (err) {
-            throw err;
-        }
-
-        res.render('search5',{"row" : row});
-
-        console.log(row);
-    });
-
-    // close the database connection
-    db.close();
-    console.log('disconnected from the in-memory SQlite database.');
-
 };
